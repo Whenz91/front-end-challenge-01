e2161eb9398aeee73f0ceeb4e363ce42d10e4910
Whenz91 <mikozoltan91@gmail.com>
2022-06-28 15:26:02 +0200

    I complated the first front-end dev challenge which is button component

---
+++ src/App.vue
@@ -0,0 +1,124 @@
+<script setup>
+  import Button from "@/components/Buttons.vue";
+
+  const sampleCodes = {
+    default: '<Button />',
+    outlineBtn: '<Button variant="outline" />',
+    textBtn: '<Button variant="text" />',
+    noShadow: '<Button disableShadow />',
+    disabledBtn: '<Button disabled />',
+    disabledTextBtn: '<Button variant="text" disabled />',
+    iconStartBtn: '<Button startIcon="local_grocery_store" />',
+    iconEndBtn: '<Button endIcon="local_grocery_store" />',
+    smBtn: '<Button size="sm" />',
+    mdBtn: '<Button size="md" />',
+    lgBtn: '<Button size="lg" />',
+    colorDefaultBtn: '<Button color="default" />',
+    colorPrimaryBtn: '<Button color="primary" />',
+    colorSecondaryBtn: '<Button color="secondary" />',
+    colorDangerBtn: '<Button color="danger" />',
+  }
+</script>
+
+<template>
+  <main>
+    <h1>Buttons</h1>
+    <pre><code>{{ sampleCodes.default }}</code></pre>
+    <Button />
+
+    <pre><code>{{ sampleCodes.outlineBtn }}</code></pre>
+    <Button variant="outline" />
+
+    <pre><code>{{ sampleCodes.textBtn }}</code></pre>
+    <Button variant="text" />
+
+    <pre><code>{{ sampleCodes.noShadow }}</code></pre>
+    <Button disableShadow color="primary" />
+
+    <ul class="flex no-list my-20">
+      <li>
+        <pre><code>{{ sampleCodes.disabledBtn }}</code></pre>
+        <Button disabled />
+      </li>
+      <li>
+        <pre><code>{{ sampleCodes.disabledTextBtn }}</code></pre>
+        <Button variant="text" disabled />
+      </li>
+    </ul>
+
+    <ul class="flex no-list my-20">
+      <li>
+        <pre><code>{{ sampleCodes.iconStartBtn }}</code></pre>
+        <Button startIcon="local_grocery_store" color="primary" />
+      </li>
+      <li>
+        <pre><code>{{ sampleCodes.iconEndBtn }}</code></pre>
+        <Button endIcon="local_grocery_store" color="primary" />
+      </li>
+    </ul>
+
+    <ul class="flex no-list my-20">
+      <li>
+        <pre><code>{{ sampleCodes.smBtn }}</code></pre>
+        <Button size="sm" color="primary" />
+      </li>
+      <li>
+        <pre><code>{{ sampleCodes.mdBtn }}</code></pre>
+        <Button size="md" color="primary" />
+      </li>
+      <li>
+        <pre><code>{{ sampleCodes.lgBtn }}</code></pre>
+        <Button size="lg" color="primary" />
+      </li>
+    </ul>
+
+    <ul class="flex no-list my-20">
+      <li>
+        <pre><code>{{ sampleCodes.colorDefaultBtn }}</code></pre>
+        <Button color="default" />
+      </li>
+      <li>
+        <pre><code>{{ sampleCodes.colorPrimaryBtn }}</code></pre>
+        <Button color="primary" />
+      </li>
+      <li>
+        <pre><code>{{ sampleCodes.colorSecondaryBtn }}</code></pre>
+        <Button color="secondary" />
+      </li>
+      <li>
+        <pre><code>{{ sampleCodes.colorDangerBtn }}</code></pre>
+        <Button color="danger" />
+      </li>
+    </ul>
+  </main>
+</template>
+
+<style>
+@import './assets/base.css';
+
+h1 {
+  font-size: 24px;
+  font-weight: 500;
+  margin-bottom: 28px;
+}
+
+code {
+  font-family: var(--ff-ubuntu);
+  font-weight: 400;
+}
+
+.flex {
+  display: flex;
+  column-gap: 125px;
+}
+.no-list {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+.my-20 {
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+
+</style>
---
+++ src/components/Buttons.vue
@@ -0,0 +1,129 @@
+<script setup>
+    defineProps({
+        variant: String,
+        disableShadow: Boolean,
+        disabled: Boolean,
+        startIcon: String,
+        endIcon: String,
+        size: String,
+        color: String
+    })
+</script>
+
+<template>
+    <button 
+        :class="[
+            {'noShadow': disableShadow},
+            variant == 'outline' ? 'outline' : '',
+            variant == 'text' ? 'text' : '',
+            size == 'sm' ? 'sm-btn' : '',
+            size == 'md' ? 'md-btn' : '',
+            size == 'lg' ? 'lg-btn' : '',
+            color == 'default' ? 'default' :'',
+            color == 'primary' ? 'primary' :'',
+            color == 'secondary' ? 'secondary' :'',
+            color == 'danger' ? 'danger' :'',
+        ]"
+        :disabled="disabled ? true : false"
+    >
+       <span v-if="startIcon" class="material-icons-outlined">{{ startIcon }}</span>
+       Default
+       <span v-if="endIcon" class="material-icons-outlined">{{ endIcon }}</span>
+    </button>
+</template>
+
+<style scoped>
+button {
+    display: flex;
+    align-items: center;
+    column-gap: 10px;
+    font-family: var(--ff-poppins);
+    font-size: 14px;
+    font-weight: 500;
+    padding: 8px 16px;
+    margin: 20px 0;
+    text-align: center;
+    color: var(--color-gray-1);
+    background-color: var(--color-gray-5);
+    border: none;
+    box-shadow: 0px 2px 3px rgba(51, 51, 51, 0.2);
+    border-radius: 6px;
+    cursor: pointer;
+}
+button:is(:hover, :focus),
+button.default:is(:hover, :focus) {
+    background-color: var(--color-gray-3);
+}
+
+button.outline {
+    border: 1px solid var(--color-primary-1);
+    background-color: var(--color-white);
+    color: var(--color-primary-1);
+    box-shadow: none;
+}
+
+button.text {
+    background-color: var(--color-white);
+    color: var(--color-primary-1);
+    box-shadow: none;
+}
+button.outline:is(:hover, :focus), 
+button.text:is(:hover, :focus) {
+    background-color: var(--color-primary-3);
+}
+
+button.noShadow {
+    box-shadow: none;
+}
+
+button:disabled {
+    background-color: var(--color-gray-5);
+    color: var(--color-gray-4);
+    cursor: not-allowed;
+}
+button.text:disabled {
+    background-color: transparent;
+    color: var(--color-gray-4);
+}
+
+.material-icons-outlined {
+    font-size: 14px;
+}
+
+button.sm-btn {
+    padding: 6px 12px;
+}
+button.md-btn {
+    padding: 8px 16px;
+}
+button.lg-btn {
+    padding: 11px 22px;
+}
+
+button.default {
+    color: var(--color-gray-1);
+    background-color: var(--color-gray-5);
+}
+button.primary {
+    color: var(--color-white);
+    background-color: var(--color-primary-1);
+}
+button.primary:is(:hover, :focus) {
+    background-color: var(--color-primary-2);
+}
+button.secondary {
+    color: var(--color-white);
+    background-color: var(--color-secondary-1);
+}
+button.secondary:is(:hover, :focus) {
+    background-color: var(--color-secondary-2);
+}
+button.danger {
+    color: var(--color-white);
+    background-color: var(--color-danger-1);
+}
+button.danger:is(:hover, :focus) {
+    background-color: var(--color-danger-2);
+}
+
+</style>
